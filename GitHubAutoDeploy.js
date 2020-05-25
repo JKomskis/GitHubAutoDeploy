2a84bb3af8712d549247dd0ab2d36f36497ae138
Joseph Komskis <joseph.komskis@gmail.com>
2020-05-25 11:06:22 -0400

    Remove old code

--- GitHubAutoDeploy.js
+++ GitHubAutoDeploy.js
@@ -55,7 +55,7 @@ function executeCommands(commands){
 }
 
 function executeCommand(commands, index){
-	exec(commands[index], (error, stdout, stderr) => {
+	exec("PATH=/bin:" + process.env.PATH + " " + commands[index], (error, stdout, stderr) => {
 		console.log('> ' + commands[index]);
 		if (error) {
 			console.error('exec error: ' + error);
@@ -68,67 +68,3 @@ function executeCommand(commands, index){
 			executeCommand(commands, index+1);
 	});
 }
-
-/*
-// app.get('/', function(req, res) {
-	// get all the flowers
-	// res.render('index.html', {root: __dirname});
-// });
-
-app.get('/api/flowers', function(req, res) {
-	// get all the flowers
-	db.all("SELECT COMNAME FROM FLOWERS", function(err, rows){
-		res.json(rows);
-	});
-});
-
-app.get('/api/flowers/:flowerCOMNAME', (req, res) => {
-	console.log(req.params.flowerCOMNAME);
-	db.all("SELECT * FROM SIGHTINGS WHERE ? = SIGHTINGS.NAME ORDER BY SIGHTED DESC", req.params.flowerCOMNAME, function(err, rows){
-		res.json(rows);
-	});
-});
-
-// Update a flower with the passed information
-app.post('/api/flowers/update', (req, res) => {
-	var oldComname = req.body.oldComname;
-	var genus = req.body.genus;
-	var species = req.body.species;
-	var comname = req.body.comname;
-
-	console.log("Updating old " + oldComname + " with genus " + genus + ", species " + species + ", and common name " + comname);
-
-	db.all("UPDATE FLOWERS SET genus = ?, species = ?, comname = ? WHERE comname = ?", genus, species, comname, oldComname, function(err, info){
-		if (err === null){
-			console.log("Update success");
-			res.end("Success");
-		} else {
-			console.log("Update failure");	
-			res.end(err);
-		}
-	});
-	
-});
-
-// Insert a new sighting with the passed information
-app.post('/api/flowers/insert', (req, res) => {
-	var name = req.body.name;
-	var person = req.body.person;
-	var location = req.body.location;
-	var sighted = req.body.sighted;
-
-	console.log("Adding sighted " + name + " found by " + person + " at " + location + " on " + sighted);
-
-	db.all("INSERT INTO Sightings (name, person, location, sighted) VALUES (?, ?, ?, ?)", name, person, location, sighted, function(err, info){
-		if (err === null){
-			console.log("Insert success");			
-			res.end("Success");
-		} else {
-			console.log("Insert failure");				
-			res.end(err);
-		}
-	});
-
-});
-
-*/
